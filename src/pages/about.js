61df695984e9547518795385628b38cef03b0ad0
Martin Gaucher <martin.gaucher@cotral.local>
2021-12-24 11:28:05 +0100

    Part 2 of Gatsby Tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from './layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi there ! I'm Martin Gaucher from Normandy dans l'Orne !</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
