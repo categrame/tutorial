da321a35e113a1524791c5966e49cdd599f831e4
Martin Gaucher <martin.gaucher@cotral.local>
2021-12-27 08:54:38 +0100

    Gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "tutorial",
+    title: "A nice Website",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      } 
+    }
   ],
 };
